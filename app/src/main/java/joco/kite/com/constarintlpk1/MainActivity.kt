84a31f5e5572b1fc8e83acc5567044e203a3e626
mesterj <mesterj@freemail.hu>
2018-08-05 00:49:34 +0200

    Initial commit

---
+++ app/src/main/java/joco/kite/com/constarintlpk1/MainActivity.kt
@@ -0,0 +1,19 @@
+package joco.kite.com.constarintlpk1
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        button.setOnClickListener(View.OnClickListener { toast("Hello anko") })
+
+
+    }
+}
